f93f0f791f45cc7e6cd13f21292548e02fc99ca4
Touhidul Islam <touhidul.islam@justice.gov.uk>
2022-09-26 16:17:32 +0100

    :alembic: configure postfix with cyrus sassl to provide smtp authentication
    it also creates a sasl testuser and adds a test script to test smtp authentication

--- smtp-relay-test/test.sh
+++ smtp-relay-test/test.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 set -e
 
-echo "This is a Test Email from SMTP Relay Server" | mail -s "Test email from SMTP Relay Server" -S mta=smtp://smtp_relay_server -S from=$TEST_SENDER_EMAIL_ADDRESS -S smtp-auth=none $O365_TEST_RECIPIENT_EMAIL_ADDRESS
-echo "This is a Test Email from SMTP Relay Server" | mail -s "Test email from SMTP Relay Server" -S mta=smtp://smtp_relay_server -S from=$TEST_SENDER_EMAIL_ADDRESS -S smtp-auth=none $GOOGLE_TEST_RECIPIENT_EMAIL_ADDRESS
-echo "This is a Test Email from SMTP Relay Server" | mail -s "Test email from SMTP Relay Server" -S mta=smtp://smtp_relay_server -S from=$TEST_SENDER_EMAIL_ADDRESS -S smtp-auth=none $OTHER_TEST_RECIPIENT_EMAIL_ADDRESS
+# echo "This is a Test Email from SMTP Relay Server" | mail -s "Test email from SMTP Relay Server" -S mta=smtp://smtp_relay_server -S from=$TEST_SENDER_EMAIL_ADDRESS -S smtp-auth=none $O365_TEST_RECIPIENT_EMAIL_ADDRESS
+# echo "This is a Test Email from SMTP Relay Server" | mail -s "Test email from SMTP Relay Server" -S mta=smtp://smtp_relay_server -S from=$TEST_SENDER_EMAIL_ADDRESS -S smtp-auth=none $GOOGLE_TEST_RECIPIENT_EMAIL_ADDRESS
+echo "This is a Test Email from SMTP Relay Server" | mail -s "Test email from SMTP Relay Server" -S mta=smtp://testuser:Password@smtp_relay_server -S from=$TEST_SENDER_EMAIL_ADDRESS $OTHER_TEST_RECIPIENT_EMAIL_ADDRESS
+echo "This is a Test Email from SMTP Relay Server" | mail -s "Test email from SMTP Relay Server" -S mta=smtp://smtp_relay_server -S smtp-auth=CRAM-MD5 -S smtp-auth-user=testuser@postfix.devl.justice.gov.
+uk -S smtp-auth-password=Password -S from=testuser@postfix.devl.justice.gov.uk tislam@live.co.uk
 
 exec "$@"
--- smtp-relay/entrypoint.sh
+++ smtp-relay/entrypoint.sh
@@ -12,6 +12,7 @@ fi
 # Create postfix folders
 mkdir -p /var/spool/postfix/
 mkdir -p /var/spool/postfix/pid
+mkdir -p /var/spool/postfix/vhosts
 
 # Disable SMTPUTF8, because libraries (ICU) are missing in Alpine
 postconf -e "smtputf8_enable = no"
@@ -29,8 +30,21 @@ postconf -e "myorigin = $RELAY_DOMAIN"
 postconf -e "relay_domains = $RELAY_DOMAIN"
 postconf -e "mydestination = $HOSTNAME.$PUBLIC_DNS_ZONE_NAME_STAFF_SERVICE, $RELAY_DOMAIN"
 
+postconf -e "home_mailbox = Maildir/"
+postconf -e "mail_spool_directory = /var/spool/postfix"
+postconf -e "smtpd_banner = $myhostname ESMTP $mail_name ($mail_version)"
+
+postconf -e "local_transport = virtual"
+postconf -e "virtual_mailbox_domains = postfix.devl.justice.gov.uk"
+postconf -e "virtual_mailbox_base = /var/spool/postfix/vhosts"
+postconf -e "virtual_mailbox_maps = lmdb:/etc/postfix/vmailbox"
+postconf -e "virtual_minimum_uid = 100"
+postconf -e "virtual_uid_maps = static:1001"
+postconf -e "virtual_gid_maps = static:1001"
+postconf -e "virtual_alias_maps = lmdb:/etc/postfix/valias"
+
 # Create transport mapping
-echo "$RELAY_DOMAIN relay:[$O365_SMART_HOST]" >> /etc/postfix/transport_maps
+echo "$RELAY_DOMAIN relay:[$O365_SMART_HOST]" > /etc/postfix/transport_maps
 postmap lmdb:/etc/postfix/transport_maps
 postconf -e "transport_maps = lmdb:/etc/postfix/transport_maps"
 
@@ -39,17 +53,31 @@ postconf -e "local_recipient_maps ="
 
 # IP ranges that are allowed to relay messages through this server
 #  13.40.249.195/32 AWS NOC
-postconf -e "mynetworks = 127.0.0.0/8, 172.0.0.0/8, 51.149.250.191/32, 13.40.249.195/32, 86.142.44.41/32, 10.0.0.0/8"
+postconf -e "mynetworks = $IP_ALLOWED_LIST"
 
 # All about security
 postconf -e "smtpd_delay_reject = yes"
 postconf -e "smtpd_helo_required = yes"
-postconf -e "smtpd_helo_restrictions = permit_mynetworks, reject_invalid_helo_hostname, permit"
-postconf -e "smtp_sasl_auth_enable = no"
+postconf -e "smtpd_sasl_auth_enable = yes"
+postconf -e "broken_sasl_auth_clients = yes"
+postconf -e "smtpd_sasl_path = smtpd"
+# postconf -e "smtpd_helo_restrictions = permit_mynetworks, permit"
+postconf -e "smtpd_relay_restrictions = permit_sasl_authenticated, permit_mynetworks, reject_unauth_destination, reject"
 postconf -e "smtp_tls_CAfile = /etc/ssl/certs/ca-certificates.crt"
-postconf -e "smtp_sasl_security_options = noanonymous"
+postconf -e "smtpd_sasl_security_options = noanonymous, noplaintext"
+postconf -e "smtpd_sasl_tls_security_options = noanonymous"
 postconf -e "smtp_use_tls = yes"
 postconf -e "smtp_tls_security_level = encrypt"
+
+# Create valias mapping
+echo "testuser@postfix.devl.justice.gov.uk testuser@postfix.devl.justice.gov.uk" > /etc/postfix/valias
+postmap lmdb:/etc/postfix/valias
+
+# Create vmailbox mapping
+echo "testuser@postfix.devl.justice.gov.uk postfix.devl.justice.gov.uk/testuser/" > /etc/postfix/vmailbox
+postmap lmdb:/etc/postfix/vmailbox
+
+postmap lmdb:/etc/postfix/aliases
 
 # Limit message size to ~50MB 
 # ExchangeOnline is 35Mb and Google Workspace 50Mb
@@ -61,7 +89,7 @@ postconf -e "remote_header_rewrite_domain = $RELAY_DOMAIN"
 postconf -e "append_dot_mydomain = yes"
 
 # Create sender canonical address mapping
-echo "$HOSTNAME@$PUBLIC_DNS_ZONE_NAME_STAFF_SERVICE $HOSTNAME@$RELAY_DOMAIN" >> /etc/postfix/sender_canonical
+echo "$HOSTNAME@$PUBLIC_DNS_ZONE_NAME_STAFF_SERVICE $HOSTNAME@$RELAY_DOMAIN" > /etc/postfix/sender_canonical
 postmap lmdb:/etc/postfix/sender_canonical
 postconf -e "sender_canonical_maps = lmdb:/etc/postfix/sender_canonical"
 
@@ -74,5 +102,6 @@ echo
 
 exec "$@"
 
+exec /usr/sbin/saslauthd -a sasldb -c -d & sleep 2
 exec postfix start-fg & sleep 2 # give postfix time to create the newest log
 exec tail -f /var/log/mail/postfix
