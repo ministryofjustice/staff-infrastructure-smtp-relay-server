097df565011f57498a6ab0a41ef0d0977a78c3cc
James Green <james.green6@justice.gov.uk>
2024-07-23 16:34:31 +0100

    test postfix service locally using ses as a relay

--- smtp-relay/entrypoint.sh
+++ smtp-relay/entrypoint.sh
@@ -13,56 +13,70 @@ fi
 mkdir -p /var/spool/postfix/
 mkdir -p /var/spool/postfix/pid
 
-# Disable SMTPUTF8, because libraries (ICU) are missing in Alpine
-postconf -e "smtputf8_enable = no"
+mkdir /etc/postfix/sasl
 
-# Log to file. Tail the logs to STDOUT below
-postconf -e "maillog_file = /var/log/mail/postfix"
+#cp /tmp/smtp-relay/sender_access /etc/postfix/sender_access
+#cp /tmp/smtp-relay/restricted_recipient_access /etc/postfix/restricted_recipient_access
+cp /tmp/smtp-relay/sasl_passwd /etc/postfix/sasl_passwd
+cp /tmp/smtp-relay/smtpd.conf /etc/postfix/sasl/smtpd.conf
 
+#cp /tmp/smtp-relay/master.cf /etc/postfix/master.cf
+cp /tmp/smtp-relay/main.cf /etc/postfix/main.cf
+chmod 600 /etc/postfix/sasl_passwd
+postmap /etc/postfix/sasl_passwd
+#postmap /etc/postfix/sender_access
+#postmap /etc/postfix/restricted_recipient_access
+
+## Disable SMTPUTF8, because libraries (ICU) are missing in Alpine
+#postconf -e "smtputf8_enable = no"
+#
+## Log to file. Tail the logs to STDOUT below
+#postconf -e "maillog_file = /var/log/mail/postfix"
+#
 # Update aliases database. It's not used, but postfix complains if the .db file is missing
 postalias /etc/postfix/aliases
-
-# All about domains
-postconf -e "myhostname = $HOSTNAME.$PUBLIC_DNS_ZONE_NAME_STAFF_SERVICE"
-postconf -e "mydomain = $RELAY_DOMAIN"
-postconf -e "myorigin = $RELAY_DOMAIN"
-postconf -e "relay_domains = $RELAY_DOMAIN"
-postconf -e "mydestination = $HOSTNAME.$PUBLIC_DNS_ZONE_NAME_STAFF_SERVICE, $RELAY_DOMAIN"
-
-# Create transport mapping
-echo "$RELAY_DOMAIN relay:[$O365_SMART_HOST]" >> /etc/postfix/transport_maps
-postmap lmdb:/etc/postfix/transport_maps
-postconf -e "transport_maps = lmdb:/etc/postfix/transport_maps"
-
-# Disable local recipients checks
-postconf -e "local_recipient_maps ="
-
-# IP ranges that are allowed to relay messages through this server (maintained in AWS SS Parameter Store)
-postconf -e "mynetworks = $IP_ALLOWED_LIST"
-
-# All about security
-postconf -e "smtpd_delay_reject = yes"
-postconf -e "smtpd_helo_required = yes"
-postconf -e "smtpd_relay_restrictions = permit_mynetworks, reject"
-postconf -e "smtp_sasl_auth_enable = no"
-postconf -e "smtp_tls_CAfile = /etc/ssl/certs/ca-certificates.crt"
-postconf -e "smtp_sasl_security_options = noanonymous"
-postconf -e "smtp_use_tls = yes"
-postconf -e "smtp_tls_security_level = encrypt"
-
-# Limit message size to ~50MB 
-# ExchangeOnline is 35Mb and Google Workspace 50Mb
-postconf -e "message_size_limit = 50000000"
-
-# Sender address rewriting
-postconf -e "masquerade_domains = $RELAY_DOMAIN"
-postconf -e "remote_header_rewrite_domain = $RELAY_DOMAIN"
-postconf -e "append_dot_mydomain = yes"
-
-# Create sender canonical address mapping
-echo "$HOSTNAME@$PUBLIC_DNS_ZONE_NAME_STAFF_SERVICE $HOSTNAME@$RELAY_DOMAIN" >> /etc/postfix/sender_canonical
-postmap lmdb:/etc/postfix/sender_canonical
-postconf -e "sender_canonical_maps = lmdb:/etc/postfix/sender_canonical"
+#
+## All about domains
+#postconf -e "myhostname = $HOSTNAME.$PUBLIC_DNS_ZONE_NAME_STAFF_SERVICE"
+#postconf -e "mydomain = $RELAY_DOMAIN"
+#postconf -e "myorigin = $RELAY_DOMAIN"
+#postconf -e "relay_domains = $RELAY_DOMAIN"
+#postconf -e "mydestination = $HOSTNAME.$PUBLIC_DNS_ZONE_NAME_STAFF_SERVICE, $RELAY_DOMAIN"
+#
+## Create transport mapping
+#echo "$RELAY_DOMAIN relay:[$O365_SMART_HOST]" >> /etc/postfix/transport_maps
+#postmap lmdb:/etc/postfix/transport_maps
+#postconf -e "transport_maps = lmdb:/etc/postfix/transport_maps"
+#
+## Disable local recipients checks
+#postconf -e "local_recipient_maps ="
+#
+## IP ranges that are allowed to relay messages through this server (maintained in AWS SS Parameter Store)
+#postconf -e "mynetworks = $IP_ALLOWED_LIST"
+#
+## All about security
+#postconf -e "smtpd_delay_reject = yes"
+#postconf -e "smtpd_helo_required = yes"
+#postconf -e "smtpd_relay_restrictions = permit_mynetworks, reject"
+#postconf -e "smtp_sasl_auth_enable = no"
+#postconf -e "smtp_tls_CAfile = /etc/ssl/certs/ca-certificates.crt"
+#postconf -e "smtp_sasl_security_options = noanonymous"
+#postconf -e "smtp_use_tls = yes"
+#postconf -e "smtp_tls_security_level = encrypt"
+#
+## Limit message size to ~50MB
+## ExchangeOnline is 35Mb and Google Workspace 50Mb
+#postconf -e "message_size_limit = 50000000"
+#
+## Sender address rewriting
+#postconf -e "masquerade_domains = $RELAY_DOMAIN"
+#postconf -e "remote_header_rewrite_domain = $RELAY_DOMAIN"
+#postconf -e "append_dot_mydomain = yes"
+#
+## Create sender canonical address mapping
+#echo "$HOSTNAME@$PUBLIC_DNS_ZONE_NAME_STAFF_SERVICE $HOSTNAME@$RELAY_DOMAIN" >> /etc/postfix/sender_canonical
+#postmap lmdb:/etc/postfix/sender_canonical
+#postconf -e "sender_canonical_maps = lmdb:/etc/postfix/sender_canonical"
 
 # Define local mail delivery location
 postconf -e "home_mailbox = Maildir/"
