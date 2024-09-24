bee6e2c8632cf3059a888a2d37c177d1b419d28e
Jahir <124062934+juddin927@users.noreply.github.com>
2024-09-24 17:53:45 +0100

    Reverting test message

--- smtp-relay-monitoring/entrypoint.sh
+++ smtp-relay-monitoring/entrypoint.sh
@@ -7,11 +7,5 @@ if [ ! -f "$FILE" ]; then
   mkdir -p "/var/log/mail"
   touch "$FILE"
 fi
-output_message() {
-  while true; do
-    echo "Failed to scrape, this is a test message"
-    sleep 300  # Sleep for 5 minutes (300 seconds)
-  done
-}
-output_message &
+
 /bin/postfix_exporter --postfix.logfile_path=$FILE
