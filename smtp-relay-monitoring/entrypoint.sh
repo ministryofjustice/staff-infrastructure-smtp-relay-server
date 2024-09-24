c863668b3d364a18b801cb1f9d48bd59ec40c358
Jahir <124062934+juddin927@users.noreply.github.com>
2024-09-24 12:56:11 +0100

    added a test message as standard output in order to test Cloudwatch metric in Postfix exporter container (#103)

--- smtp-relay-monitoring/entrypoint.sh
+++ smtp-relay-monitoring/entrypoint.sh
@@ -7,5 +7,11 @@ if [ ! -f "$FILE" ]; then
   mkdir -p "/var/log/mail"
   touch "$FILE"
 fi
-
+output_message() {
+  while true; do
+    echo "Failed to Scrape, this is a test message"
+    sleep 300  # Sleep for 5 minutes (300 seconds)
+  done
+}
+output_message &
 /bin/postfix_exporter --postfix.logfile_path=$FILE
